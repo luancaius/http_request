9dfeb5162204e5cb9cbd960eda3e73a50b5fe2ed
luancaius <luancaius@outlook.com>
2020-03-10 00:05:32 -0400

    feat: testing get

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
-function httpSimpleRequest(input){
-    return input.HttpMethod;
+const axios = require('axios');
+
+async function request(input){
+    let startTime, endTime;
+    startTime = new Date();
+
+    let response = {
+        httpHeader:0,
+        timeInMilisec:0,
+        data:{}
+    };
+
+    const url=input.domain+"/"+input.path
+    await axios.get(url, {
+        params: input.params
+      })
+      .then(function (res) {
+        response.data = res.data
+        response.httpHeader = res.status
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+      .then(function () {
+        // always executed
+      });  
+    endTime = new Date();
+    response.timeInMilisec = endTime - startTime; 
+
+    return response;
 }
 
-module.exports = httpSimpleRequest;
+module.exports = request;
